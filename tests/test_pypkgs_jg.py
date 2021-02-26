a086bf1fd816d20f4da7cf3e54c82a59292a9ef7
Jayme Gordon <jaymegordo@gmail.com>
2021-02-26 13:38:14 -0800

    Add catbind tests

--- tests/test_pypkgs_jg.py
+++ tests/test_pypkgs_jg.py
@@ -1,5 +1,17 @@
+import pandas as pd
 from pypkgs_jg import __version__
-from pypkgs_jg import pypkgs_jg
+from pypkgs_jg import pypkgs_jg as pj
+
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    c = pd.Categorical(["macgrubie", "dont", "play", "like", "homie"])
+    cat = pj.catbind(a, b, c)
+
+    assert (cat.codes == [1,  5, 12,  4,  0,  7, 11,  2,  9,  3, 10,  8,  6]).all()
+    
+    assert (cat.categories == ['but', 'character', 'counts', 'dont', 'eyeballs', 'hits', 'homie', 'integer', 'like', 'macgrubie', 'play', 'where it', 'your']).all()
